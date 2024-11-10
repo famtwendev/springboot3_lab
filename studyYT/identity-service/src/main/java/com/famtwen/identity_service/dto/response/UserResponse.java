4e04e9c58c6b7040f5e9135fded32964bd5869ef
Nguyen Pham Tuyen <phamtuyen121314@gmail.com>
2024-11-10 16:57:17 +0700

    first commit

---
+++ studyYT/identity-service/src/main/java/com/famtwen/identity_service/dto/response/UserResponse.java
@@ -0,0 +1,22 @@
+package com.famtwen.identity_service.dto.response;
+
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserResponse {
+    String id;
+    String username;
+    String password;
+    String firstName;
+    String lastName;
+    LocalDate dob;
+}
+// sau khi tao thi tao mapper
