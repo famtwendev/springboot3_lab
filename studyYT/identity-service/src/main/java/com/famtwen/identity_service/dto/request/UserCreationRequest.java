8d28e775850c8d7f4470ce9d3603e243e1791337
Nguyen Pham Tuyen <phamtuyen121314@gmail.com>
2024-11-17 23:11:31 +0700

    #14 Validation bằng custom annotation

--- studyYT/identity-service/src/main/java/com/famtwen/identity_service/dto/request/UserCreationRequest.java
+++ studyYT/identity-service/src/main/java/com/famtwen/identity_service/dto/request/UserCreationRequest.java
@@ -1,5 +1,6 @@
 package com.famtwen.identity_service.dto.request;
 
+import com.famtwen.identity_service.validator.DobConstraint;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -19,6 +20,8 @@ public class UserCreationRequest {
     String password;
     String firstName;
     String lastName;
+
+    @DobConstraint(min = 18, message = "INVALID_DOB")
     LocalDate dob;
 }
 
--- studyYT/identity-service/src/main/java/com/famtwen/identity_service/dto/request/UserUpdateRequest.java
+++ studyYT/identity-service/src/main/java/com/famtwen/identity_service/dto/request/UserUpdateRequest.java
@@ -3,6 +3,7 @@ package com.famtwen.identity_service.dto.request;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.famtwen.identity_service.validator.DobConstraint;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -15,6 +16,7 @@ public class UserUpdateRequest {
     String password;
     String firstName;
     String lastName;
+    @DobConstraint(min = 18, message = "INVALID_DOB")
     LocalDate dob;
     List<String> roles;
 }
